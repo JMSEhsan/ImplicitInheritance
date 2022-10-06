392bc157e62bdb96c07f41cb2eb87071b69b7b4f
Ehsan Jalali <JMSEhsan@gmail.com>
2022-10-06 00:05:41 -0600

    commit

---
+++ Super.py
@@ -0,0 +1,18 @@
+#Shaw, Zed, "Learn Python 3 The Hard Way" Addison-Wesly, 2017
+
+class Parent(object):
+    
+    def altered(self):
+        print("Parent altered()")
+class Child(Parent):
+
+    def altered(slef):
+        print("Child, before Parent altered()")
+        super(Child, self).altered()
+        print("Child, after parent altered()")
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
